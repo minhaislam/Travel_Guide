ab29699dea964c091a987f361a3ee67bbea2d1f8
minhaislam <minhajislam95@gmail.com>
2020-04-11 00:36:03 +0600

    three user Admin,Scout,General User

---
+++ resources/views/registration/index.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Login Page</title>
+</head>
+<body>
+	<h1>Registration Page</h1>
+	<form method="post">
+		<!-- @csrf -->
+		<!--{{ csrf_field()}} -->	
+		<input type="hidden" name="_token" value="{{csrf_token()}}">
+		Email: <input type="text" name="email" > <br>
+		Username: <input type="text" name="user_name" > <br>
+		Password: <input type="password" name="password" ><br>
+		type:<br> <input type="radio" name="type" value="admin" >Admin||<input type="radio" name="type" value="scout" >Scout||<input type="radio" name="type" value="user" >General User <br>
+		<input type="submit" name="submit" value="Submit" >
+	</form>
+	<a href="/register">Register</a>
+	<h3>{{session('msg')}}</h3>
+</body>
+</html>
